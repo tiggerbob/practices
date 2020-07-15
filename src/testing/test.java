a3ee0173689d9d14b7d3d2cbbbc6f53d5252f09a
koo8 <kooeight@gmail.com>
2020-07-15 11:35:38 -0500

    armstrong number calculator w/ static methods

--- src/testing/test.java
+++
@@ -1,8 +0,0 @@
-package testing;
-
-public class test {
-    public static void main(String[] args) {
-        int n = 200;
-        System.out.println(n>>3);
-    }
-}
--- src/thread/synchronize/synchronizeThread.java
+++ src/thread/synchronize/synchronizeThread.java
@@ -2,29 +2,47 @@ package thread.synchronize;
 /*
 * methods for threads:
 * start(): starts the thread
-* stop(): force stops the thread (highly unreccomended)
+* stop(): force stops the thread (highly unrecommended)
 * sleep(): pause/sleep the thread for a certain number of milliseconds
 * interrupt(): interrupts the thread
 * interrupted(): checks if the thread has been interrupted
 * join(): waits for the completion of a separate thread
 * synchronized: keyword - makes methods synchronized with each other
 * execute(): executor interface - replace normal start thread with .execute
+* yield(): changes the state of the thread from Running to Enabled
 * */
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 class synchronizeThread {
 
-    public static void main(String[] args) throws InterruptedException {
-        String[] importantInfo = {
-                "Mares eat oats",
-                "Does eat oats",
-                "Little lambs eat ivy",
-                "A kid will eat ivy too"
-        };
+    public static int count = 0;
 
-        for (String s : importantInfo) {
-            Thread.sleep(4000);
-            System.out.println(s);
+    synchronized public static void count() {
+        synchronized (synchronizeThread.class){
+           count++;
         }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 100; i++) {
+            Thread threads = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    count();
+                }
+            });
+
+            threads.start();
+//            System.out.println(count);
+//            threads.sleep(300);
+//            threads.join();
+        }
+        Thread.sleep(1000 );
+        System.out.println(count);
+
     }
 }
 
